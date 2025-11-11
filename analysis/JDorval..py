167f3b99136c9e5a694b6ca6fb9214a1503d021d
JustinD2016 <jdorval2016@gmail.com>
2025-11-11 08:47:30 -0500

    Finsihed getting ratios and clean data

--- analysis/JDorval..py
+++ analysis/JDorval..py
@@ -2,4 +2,4 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 data = pd.read_csv(r'data\bank_innovation_dataset_FINAL.csv')
-print(data.head(),data.shape)
+print("Sparceness of the dataset: ", len(data[data.isnull() > 0.9])/len(data))
