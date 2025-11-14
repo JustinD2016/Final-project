824fbc0a284ea6d42ce8a833f16befbb7f207093
JustinD2016 <jdorval2016@gmail.com>
2025-11-14 12:28:44 -0500

    Deleted the python file, I used the notebooke

--- analysis/JDorval..py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-data = pd.read_csv(r'data\bank_innovation_dataset_FINAL.csv')
-print("Sparceness of the dataset: ", len(data[data.isnull() > 0.9])/len(data))
